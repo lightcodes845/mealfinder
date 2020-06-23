b25659ab14542f2cdfa043d7361e28f8a293e4f8
darefalola7 <darefalola7@gmail.com>
2020-06-23 11:31:12 +0100

    new update pub

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa2.enc # Allow read access to the private key
-ssh-add .travis/id_rsa2.enc # Add the private key to SSH
+
 
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
